d74f07fd2c9edfe3309c6ea132c58b34ccb849f6
FireSamQC <samuelbrassard86@gmail.com>
2024-10-06 20:52:31 -0400

    Add files via upload

---
+++ lab2_commerce.cpp
@@ -0,0 +1,27 @@
+//Partie en-tête
+//Nom : Samuel Brassard
+//Programme : Labo 2 - Commande
+//Date de création : 03/10/2024
+//Date de dernière modification : 03/10/2024
+
+//Clauses d'inclusion
+#include <iostream>
+#include "BonCommande.h"
+#include "Reception.h"
+#include "RapportAssuranceQualite.h"
+#include "Produit.h"
+#include "Inventaire.h"
+#include "Fournisseur.h"
+#include "Entreprise.h"
+#include "Entrepot.h"
+#include "Compteur.h"
+#include "Composant.h"
+#include "Commande.h"
+#include "Client.h"
+#include "hasard.h"
+
+using namespace std;
+
+int main()
+{
+}
