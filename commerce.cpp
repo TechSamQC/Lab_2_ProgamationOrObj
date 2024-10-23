3df34dc45e46ce62ba9c19d403dd23be1651c489
TechSamQC <samuelbrassard86@gmail.com>
2024-10-22 23:33:59 -0400

    Add files via upload

--- commerce.cpp
+++ commerce.cpp
@@ -2,7 +2,7 @@
 //Nom : Samuel Brassard
 //Programme : Labo 2 - Commerce
 //Date de création : 03/10/2024
-//Date de dernière modification : 10/10/2024
+//Date de dernière modification : 22/10/2024
 
 //Clauses d'inclusion
 #include <iostream>
@@ -38,7 +38,6 @@ int Compteur::constructeurCopie = 0;
 int Compteur::destructeur = 0;
 
 void afficherInformation();
-void commerce();
 void testComposants();
 void testProduits();
 void testCommandes();
@@ -47,6 +46,10 @@ void testFournisseur();
 void testEntrepot();
 void testBonCommande();
 void testReception();
+void testClient();
+void testEntreprise();
+void testRapportAQ();
+void commerce();
 
 int main()
 {
@@ -57,9 +60,11 @@ int main()
 	//testFournisseur();
 	//testEntrepot();
 	//testBonCommande();
-	testReception();
-	
-	//commerce();
+	//testReception();
+	//testClient();
+	//testEntreprise();
+	//testRapportAQ();
+	commerce();
 	afficherInformation();
 	return 0;
 }
@@ -292,7 +297,88 @@ void testReception()
 	delete maCommande;
 }
 
+void testClient()
+{
+	Client client("test", 98);
+	Commande commande = client.getCommande(0);
+	BonCommande* bonCommande = new BonCommande(128, &commande);
+	bonCommande->ajouterProduits(3, 0);
+	bonCommande->ajouterProduits(3, 1);
+	bonCommande->ajouterProduits(3, 2);
+	bonCommande->ajouterProduits(3, 3);
+	bonCommande->ajouterProduits(3, 4);
+	Reception* reception = new Reception(bonCommande);
+	/* Creation des composants et des produits pour le calcul de la qualité */
+	Composant1* c1 = new Composant1(98, 45);
+	Composant2* c2 = new Composant2(96, 33);
+	Composant3* c3 = new Composant3(22);
+	Composant4* c4 = new Composant4(100);
+	Composant5* c5 = new Composant5(2, 77, 100);
+	ProduitA produitA;
+	produitA.ajouterComposant(c1);
+	produitA.ajouterComposant(c2);
+	produitA.ajouterComposant(c3);
+	ProduitB produitB;
+	produitB.ajouterComposant(c4);
+	produitB.ajouterComposant(c5);
+	ProduitC produitC;
+	produitC.ajouterComposant(new Composant(*c1));
+	ProduitD produitD;
+	produitD.ajouterComposant(new Composant(*c2));
+	ProduitE produitE;
+	produitE.ajouterComposant(new Composant(*c3));
+	reception->ajouterProduit(new ProduitA(produitA), 0);
+	reception->ajouterProduit(new ProduitA(produitA), 0);
+	reception->ajouterProduit(new ProduitA(produitA), 0);
+	reception->ajouterProduit(new ProduitB(produitB), 1);
+	reception->ajouterProduit(new ProduitB(produitB), 1);
+	reception->ajouterProduit(new ProduitB(produitB), 1);
+	reception->ajouterProduit(new ProduitC(produitC), 2);
+	reception->ajouterProduit(new ProduitC(produitC), 2);
+	reception->ajouterProduit(new ProduitC(produitC), 2);
+	reception->ajouterProduit(new ProduitD(produitD), 3);
+	reception->ajouterProduit(new ProduitD(produitD), 3);
+	reception->ajouterProduit(new ProduitD(produitD), 3);
+	reception->ajouterProduit(new ProduitE(produitE), 4);
+	reception->ajouterProduit(new ProduitE(produitE), 4);
+	reception->ajouterProduit(new ProduitE(produitE), 4);
+	client.recevoirCommande(reception);
+	cout << client << endl;
+	cout << client.getCommande(0).getNoCommande() << endl;
+	cout << client.getReception()->getBonCommande()->getCommande()->getNoCommande() << endl;
+	cout << client.getSatisfactionProduit(0) << endl;
+	client.consommerProduits();
+}
+
+void testEntreprise()
+{
+	Entreprise* entreprise = new Entreprise();
+	delete entreprise;
+}
+
+void testRapportAQ()
+{
+	RapportAssuranceQualite* rapport = new RapportAssuranceQualite();
+	delete rapport;
+}
+
 void commerce()
 {
-	// VOTRE CODE DE PROJET SERA ICI
+	Entreprise entreprise;
+	Client client("ROGER", 90);
+	cout << client << endl;
+	for (int x = 0; x < 4; x++)
+	{
+		Commande commande(client.getCommande(x));
+		cout << commande << endl;
+		BonCommande* bonCommande = entreprise.traiterCommande(commande);
+		cout << *bonCommande << endl;
+		Reception* reception = entreprise.recupererCommande(bonCommande);
+		cout << *reception << endl;
+		client.recevoirCommande(reception);
+		RapportAssuranceQualite aq;
+		entreprise.validerSatisfaction(aq, client);
+		cout << aq.genererRapport(client) << endl;
+		client.consommerProduits();
+	}
 }
